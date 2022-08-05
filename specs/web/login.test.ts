3de9ec8564babeac5b8a28115c308895bccacb17
ANNA MATKOVSKAYA <anuta07.05@gmail.com>
2022-08-06 02:40:33 +0300

    debugging tests

--- specs/web/login.test.ts
+++ specs/web/login.test.ts
@@ -4,15 +4,15 @@ import { Link, webPages } from '../../page_objects'
 
 describe('Test for test', function () {
   beforeEach(async () => {
-    // await browser.url('http://20.0.59.108')
-    // const { deviceAllPage } = webPages()
-    // await deviceAllPage.transfer(Link.loginPage)
+
   })
   afterEach(async () => {})
 
   it('Test for test', async () => {
-    const { loginPage } = webPages()
-    await loginPage.transfer(Link.loginPage)
+    const link = 'http://20.0.59.108/login'
+    await browser.url(link)
+    const { deviceAllPage, loginPage } = webPages()
+    // await deviceAllPage.transfer(Link.loginPage)
     // await originPage.transfer(Link.loginPage)
     await browser.pause(3000)
     loginPage.login('admin')
--- specs/web/start.test.ts
+++ specs/web/start.test.ts
@@ -6,16 +6,16 @@ import { Link, webPages } from "../../page_objects";
 
 describe('Test for test', function () {
   beforeEach(async () => {
-    const { deviceAllPage } = webPages()
-    await deviceAllPage.start()
+    // const { deviceAllPage } = webPages()
+    // await deviceAllPage.start()
   })
   afterEach(async () => {
 
   })
 
   it('Test for test', async () => {
-    // const link = 'http://20.0.59.108'
-    // await browser.url(link)
+    const link = 'http://20.0.59.108'
+    await browser.url(link)
     await browser.pause(3000)
     const header = await $('h5')
     expect(await header.getText(), 'Go to the homePage').to.contain(deviceAllData.header)
